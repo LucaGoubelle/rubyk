4d54eb7138d304ed0c6007c997ce887564a680a6
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-01 16:01:26 +0200

    Add some missing cases in first corners processors

--- solver2x2/processors/firstCorners/wbo_processor.rb
+++ solver2x2/processors/firstCorners/wbo_processor.rb
@@ -14,8 +14,12 @@ class WBOProcessor
             "up_front_left::orange_blue_white" => "L' U' L",
 
             "up_back_left::white_blue_orange" => "U' y' "+CubeAlgorithms::ELEVATOR+" y",
+            "up_back_left::orange_white_blue" => "U' L' U' L",
+            "up_back_left::blue_orange_white" => "U2 L' U L",
             
             "up_back_right::white_orange_blue" => "U2 y' "+CubeAlgorithms::ELEVATOR+" y",
+            "up_back_right::blue_white_orange" => "L' U2 L",
+            "up_back_right::orange_blue_white" => "U2 L' U' L",
 
             "down_front_left::orange_white_blue" => "L' U L U' L' U L",
             "down_front_left::blue_orange_white" => "L' U' L U L' U' L"
--- solver2x2/processors/firstCorners/wgo_processor.rb
+++ solver2x2/processors/firstCorners/wgo_processor.rb
@@ -5,9 +5,20 @@ class WGOProcessor
     def initialize()
         @data = {
             "up_front_left::white_green_orange" => "L2",
+            "up_front_left::orange_white_green" => "U L U L'",
+            "up_front_left::green_orange_white" => "U L'",
+
             "up_front_right::white_orange_green" => "U L2",
+            "up_front_right::green_white_orange" => "U2 L'",
+            "up_front_right::orange_green_white" => "U2 L U L'",
+            
             "up_back_right::white_green_orange" => "U2 L2",
+            "up_back_right::orange_white_green" => "U' L U L'",
+            "up_back_right::green_orange_white" => "U' L'",
+            
             "up_back_left::white_orange_green" => "U' L2",
+            "up_back_right::green_white_orange" => "L'",
+            "up_back_right::orange_green_white" => "L U L'",
 
             "down_front_left::white_orange_green" => "L2 U' L2",
             "down_front_left::green_white_orange" => "L",

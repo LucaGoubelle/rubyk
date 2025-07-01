df695497d807a303a2793dc3081823a91eceadf2
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-01 10:54:24 +0200

    Add cases in processors (3 first corners)

--- solver2x2/processors/firstCorners/wgr_processor.rb
+++ solver2x2/processors/firstCorners/wgr_processor.rb
@@ -1,9 +1,18 @@
+require "./solver_helpers/CubeAlgorithms.rb"
 
 class WGRProcessor
     attr_reader :data
 
     def initialize()
-        @data = {}
+        @data = {
+            "up_front_right::white_green_red" => "U' y "+CubeAlgorithms::ELEVATOR+" y'",
+            "up_front_left::white_red_green" => "U2 y "+CubeAlgorithms::ELEVATOR+" y'",
+            "up_back_right::white_red_green" => "y "+CubeAlgorithms::ELEVATOR+" y'",
+            "up_back_left::white_green_red" => "U y "+CubeAlgorithms::ELEVATOR+" y'",
+
+            "down_front_left::white_green_red" => "L2 y R2 y'",
+            "down_back_left::white_red_green" => "L D L' D'"
+        }
     end
 
     def process(inputData)

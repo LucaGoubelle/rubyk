8ecb060909f0a7d0c2288dd0f929431bc08b0705
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-02 10:34:30 +0200

    Add Y perm cases in PLL Processor

--- solver2x2/processors/lastFace/pll_processor.rb
+++ solver2x2/processors/lastFace/pll_processor.rb
@@ -9,7 +9,13 @@ class PLLProcessor
             "BR_GB_RG_OO" => CubeAlgorithms::T_PERM,
             "RG_0R_GO_BB" => CubeAlgorithms::T_PERM,
             "GO_BG_OB_RR" => CubeAlgorithms::T_PERM,
-            "OB_RO_BR_GG" => CubeAlgorithms::T_PERM
+            "OB_RO_BR_GG" => CubeAlgorithms::T_PERM,
+
+            # Y cases
+
+            "BG_OR_GB_RO" => CubeAlgorithms::Y_PERM,
+            "GB_RO_BG_OR" => CubeAlgorithms::Y_PERM,
+            "RO_BG_OR_GB" => CubeAlgorithms::Y_PERM
         }
     end
     

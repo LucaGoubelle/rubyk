e17279ad6670d3fce3f9e98b5d5add8bf58ca65b
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-03 13:02:36 +0200

    Implement oll / pll handler in solver 3x3

--- solver3x3/handlers/oll_handler.rb
+++ solver3x3/handlers/oll_handler.rb
@@ -1,8 +1,22 @@
+require "./rubyklib/move/mover.rb"
+require "./solver_helpers/advanced/OLLScanner.rb"
+require "./solver3x3/processors/lastFace/oll_processor.rb"
 
 class OLLHandler
+    attr_reader :mover
+    attr_reader :scanner
+    attr_reader :ollProcessor
+
+    def initialize()
+        @mover = Mover.new()
+        @scanner = OLLScanner.new()
+        @ollProcessor = OLLProcessor.new()
+    end
 
     def handle(cube)
-        # todo: implement this
+        ollConfig = @scanner.scanOLL(cube)
+        sequence = @ollProcessor.process(ollConfig)
+        cube = @mover.multiMoves(cube, sequence)
         return cube
     end
 
--- solver3x3/handlers/pll_handler.rb
+++ solver3x3/handlers/pll_handler.rb
@@ -1,8 +1,39 @@
+require "./rubyklib/move/mover.rb"
+require "./solver_helpers/advanced/PLLScanner.rb"
+require "./solver3x3/processors/lastFace/pll_processor.rb"
 
 class PLLHandler
+    attr_reader :mover
+    attr_reader :scanner
+    attr_reader :pllProcessor
+
+    def initialize()
+        @mover = Mover.new()
+        @scanner = PLLScanner.new()
+        @pllProcessor = PLLProcessor.new()
+    end
 
     def handle(cube)
-        # todo: implement this
+        cube = handlePLL(cube)
+        cube = handleAUF(cube)
+        return cube
+    end
+
+    private handlePLL(cube)
+        pllConfig = @scanner.scanPLL(cube)
+        sequence = @pllProcessor.process(pllConfig)
+        cube = @mover.multiMoves(cube, sequence)
+        return cube
+    end
+
+    private handleAUF(cube)
+        color = cube.front[0][0]
+        hmap = {
+            "red" => "U'",
+            "green" => "U2",
+            "orange" => "U"
+        }
+        cube = if hmap.key?(color) then @mover.multiMoves(cube, hmap[color]) else cube end
         return cube
     end
 
